ad97d51d870127279a5ce4a80f3e5ac2c91e6b70
sefu_io <zephaniahsefuoduori@gmail.com>
2017-01-21 01:13:41 -0800

    secon-commit

--- app/src/main/java/com/afyaplan/afya_plan/MainActivity.java
+++ app/src/main/java/com/afyaplan/afya_plan/MainActivity.java
@@ -7,8 +7,16 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText firstname, lastname, phone, idNumber, dob, password;
-    private Button register_user;
+    EditText firstname, lastname, phone, idNumber, dob, password;
+    Button btn_register_user;
+    public static final String REGISTER_URL = "URL for the API";
+
+    public static final String KEY_FIRSTNAME = "firstname";
+    public static final String KEY_LASTNAME = "lastname";
+    public static final String KEY_PHONE = "phone";
+    public static final String KEY_IDNUMBER = "id";
+    public static final String KEY_DOB = "dob";
+    public static final String KEY_PASSWORD = "password";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +27,23 @@ public class MainActivity extends AppCompatActivity {
         phone = (EditText) findViewById(R.id.editTextPhone);
         idNumber = (EditText) findViewById(R.id.editTextId);
         dob = (EditText) findViewById(R.id.editTextDob);
-        firstname = (EditText) findViewById(R.id.editTextPassword);
-        register_user = (Button) findViewById(R.id.buttonRegister);
-        register_user.setOnClickListener(new View.OnClickListener() {
+        password = (EditText) findViewById(R.id.editTextPassword);
+        btn_register_user = (Button) findViewById(R.id.buttonRegister);
+        btn_register_user.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                registerUser();
             }
         });
     }
+
+    public void registerUser() { //get data from the fields
+        final String stringFirstName = firstname.getText().toString().trim();
+        final String stringLastName = lastname.getText().toString().trim();
+        final String stringPhone = phone.getText().toString().trim();
+        final String stringID = idNumber.getText().toString().trim();
+        final String stringDob = dob.getText().toString().trim();
+        final String stringPassword = password.getText().toString().trim();
+    }
+
 }
