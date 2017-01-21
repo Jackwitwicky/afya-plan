daa2189b405daa05da2f5ef14afef9871c784d89
sefu_io <zephaniahsefuoduori@gmail.com>
2017-01-21 00:52:48 -0800

    first commit

--- app/src/main/java/com/afyaplan/afya_plan/MainActivity.java
+++ app/src/main/java/com/afyaplan/afya_plan/MainActivity.java
@@ -1,13 +1,30 @@
 package com.afyaplan.afya_plan;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText firstname, lastname, phone, idNumber, dob, password;
+    private Button register_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firstname = (EditText) findViewById(R.id.editTextFirstName);
+        lastname = (EditText) findViewById(R.id.editTextLastName);
+        phone = (EditText) findViewById(R.id.editTextPhone);
+        idNumber = (EditText) findViewById(R.id.editTextId);
+        dob = (EditText) findViewById(R.id.editTextDob);
+        firstname = (EditText) findViewById(R.id.editTextPassword);
+        register_user = (Button) findViewById(R.id.buttonRegister);
+        register_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
     }
 }
