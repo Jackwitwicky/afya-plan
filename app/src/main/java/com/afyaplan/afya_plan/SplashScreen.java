74e268918bf9849405a4f4517aa468e326fb0163
kelvinoff <kelvinofula19@gmail.com>
2017-01-24 20:37:13 +0300

    update UI

---
+++ app/src/main/java/com/afyaplan/afya_plan/SplashScreen.java
@@ -0,0 +1,45 @@
+package com.afyaplan.afya_plan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import me.wangyuwei.particleview.ParticleView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    //private ParticleView mPv1;
+    //private ParticleView mPv2;
+     private ParticleView mPv3;
+    // private ParticleView mPv4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //mPv1 = (ParticleView) findViewById(R.id.pv_1);
+        //mPv2 = (ParticleView) findViewById(R.id.pv_2);
+        mPv3 = (ParticleView) findViewById(R.id.pv_3);
+        //mPv4 = (ParticleView) findViewById(R.id.pv_4);
+
+        mPv3.setOnParticleAnimListener(new ParticleView.ParticleAnimListener() {
+            @Override
+            public void onAnimationEnd() {
+                Intent IntroIntent = new Intent(getApplicationContext(), IntroActivity.class);
+                startActivity(IntroIntent);
+            }
+        });
+
+        mPv3.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // mPv1.startAnim();
+                //mPv2.startAnim();
+                 mPv3.startAnim();
+                // mPv4.startAnim();
+            }
+        }, 200);
+
+    }
+}
